1270d71fe042a860e3d7b460e811379f3275514d
Ritabrata Das <dasritabrata5@gmail.com>
2021-01-30 20:14:04 +0530

    Update urls.py

--- ReviewBook/urls.py
+++ ReviewBook/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from accounts.views import registration_view, login_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('movies/', include('movies.urls')),
@@ -11,8 +13,11 @@ urlpatterns = [
     path('', include('base.urls')),
     path('', include('accounts.urls')),
     path('oauth/', include('social_django.urls', namespace='social')),
+    path('register/', registration_view, name="register"),
+    path('login/', login_view, name="login"),
 ]
 
 if settings.DEBUG:
     urlpatterns +=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
